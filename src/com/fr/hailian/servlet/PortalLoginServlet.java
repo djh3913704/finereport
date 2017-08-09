690296354bc2bea52485694f4aed3c9938a074ca
Tom <344548940@qq.com>
2017-08-09 14:21:52 +0800

    portal单点登录

--- src/com/fr/hailian/servlet/PortalLoginServlet.java
+++ src/com/fr/hailian/servlet/PortalLoginServlet.java
@@ -1,6 +1,7 @@
 package com.fr.hailian.servlet;
 
 import java.io.IOException;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -8,7 +9,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONObject;
 
+import com.fr.fs.base.entity.User;
+import com.fr.fs.control.UserControl;
 import com.fr.hailian.util.BaseServlet;
+import com.fr.hailian.util.Constants;
+import com.fr.hailian.util.PortalService;
+import com.fr.hailian.util.RoleUtil;
 /**
  * 
  * @className PortalLoginServlet.java
@@ -55,8 +61,24 @@ public class PortalLoginServlet extends BaseServlet {
 		HttpServletRequest hrequest = (HttpServletRequest)request;//web资源
 		String token=hrequest.getParameter("token");
 		String redictUrl=hrequest.getParameter("redictUrl");
-		
-		r.put("msg", "");
+		//根据token获取用户信息
+		Map<String,Object> result = PortalService.getUserInfoByToken(token, Constants.PORTAL_FROM);
+		String name = (String) result.get("name");//获取用户名，需进一步确认
+		//String name = "tom";
+		try {
+			User user = UserControl.getInstance().getByUserName(name);//获取用户对象
+			if(RoleUtil.judgeAuxiliaryRole(user)){
+				//生成登陆凭证
+				RoleUtil.loginCMD(hrequest, response);
+				r.put("fail", false);
+				r.put("msg", "单点登录成功");
+			}else{
+				r.put("fail", true);
+				r.put("msg", "该用户没有辅助决策系统权限，请联系管理员!");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		responseOutWithJson(response, r);
 	}
 
